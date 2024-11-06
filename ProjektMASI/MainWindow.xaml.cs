734e9e587e956d3f687647c8f98c61cd3af91a54
CptTemplar1 <michal2001k@op.pl>
2024-11-06 19:42:29 +0100

    Wstępnie działające skalowanie linii

--- ProjektMASI/MainWindow.xaml.cs
+++ ProjektMASI/MainWindow.xaml.cs
@@ -35,5 +35,81 @@ namespace ProjektMASI
         {
 
         }
+
+        //metoda rozszerzająca pola tekstowe dopasowując je do dłuższego tekstu
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (sender is TextBox textBox)
+            {
+                // Mierzenie szerokości tekstu w TextBox
+                var formattedText = new FormattedText(
+                    textBox.Text,
+                    System.Globalization.CultureInfo.CurrentCulture,
+                    FlowDirection.LeftToRight,
+                    new Typeface(textBox.FontFamily, textBox.FontStyle, textBox.FontWeight, textBox.FontStretch),
+                    textBox.FontSize,
+                    Brushes.Black,
+                    new NumberSubstitution(),
+                    1);
+
+                // Dodanie marginesu do szerokości i przypisanie do szerokości TextBox
+                textBox.Width = formattedText.Width + 10; // Dodajemy 10 dla lepszego odstępu
+            }
+        }
+
+        private void UpdatePath(object sender, SizeChangedEventArgs e)
+        {
+            // Aktualizuje górną linię (nawias) dla pól tekstowych obok siebie
+            var path = (Path)((Grid)sender).Children[0];
+            double width = e.NewSize.Width - 20;
+            path.Data = Geometry.Parse(string.Format(
+                System.Globalization.CultureInfo.InvariantCulture,
+                "M 10,0 C 10,0 {0},-20 {1},0",
+                width / 2, width + 10));
+        }
+
+        private void UpdateVerticalPath(object sender, SizeChangedEventArgs e)
+        {
+            // Aktualizuje lewą linię (nawias) dla pól tekstowych jedno pod drugim
+            var path = (Path)((Grid)sender).Children[0];
+            double height = e.NewSize.Height - 20;
+            path.Data = Geometry.Parse(string.Format(
+                System.Globalization.CultureInfo.InvariantCulture,
+                "M 0,10 C 0,10 -20,{0} 0,{1}",
+                height / 2, height + 10));
+        }
+
+
+        private void StackPanel_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            // Sprawdzamy, czy LeftTextPanel jest przypisany, zanim spróbujemy uzyskać jego wysokość.
+            if (LeftTextPanel != null && LeftPath != null)
+            {
+                // Ustawiamy wysokość LeftPath na wysokość LeftTextPanel
+                LeftPath.Height = LeftTextPanel.ActualHeight;
+
+                // Szerokość LeftPath może być ustawiona na jakąś stałą wartość lub na podstawie szerokości LeftTextPanel.
+                // Na przykład tutaj używamy połowy szerokości LeftTextPanel.
+                LeftPath.Width = LeftTextPanel.ActualWidth / 2;
+
+                // Dostosowanie danych geometrycznych Path, aby linia była skalowana poprawnie
+                LeftPath.Data = new PathGeometry(new PathFigureCollection
+        {
+            new PathFigure
+            {
+                StartPoint = new Point(0, 0),
+                Segments = new PathSegmentCollection
+                {
+                    // Dopasowanie parametrów krzywej, aby obejmowała tekst
+                    new QuadraticBezierSegment
+                    {
+                        Point1 = new Point(LeftPath.Width / 2, -LeftPath.Height / 2),
+                        Point2 = new Point(LeftPath.Width, 0)
+                    }
+                }
+            }
+        });
+            }
+        }
     }
 }
